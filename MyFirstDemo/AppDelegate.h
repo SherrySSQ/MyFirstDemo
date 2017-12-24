87b4eefeb0c1e3a40817536d90aa0c5841210512
孙少青 <sunshaoqing@sunshaoqingdeMacBook-Pro.local>
2017-12-24 15:52:04 +0800

    Initial Commit

---
+++ MyFirstDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyFirstDemo
+//
+//  Created by 孙少青 on 2017/12/24.
+//  Copyright © 2017年 孙少青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyFirstDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyFirstDemo
+//
+//  Created by 孙少青 on 2017/12/24.
+//  Copyright © 2017年 孙少青. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
